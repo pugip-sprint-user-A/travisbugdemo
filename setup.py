bf8f78eac34f01abc4165e9b0cc36b75ac102d1e
john <john@saponara.net>
2017-01-08 17:07:50 -0500

    added README.md to look pretty on github.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = read('README')
 
 setup(
     name='travisdemo',
-    version='0.1.2',
+    version='0.1.3',
     url='http://github.com/jsaponara/travisdemo/',
     license="Affero GNU General Public License v3",
     author='John Saponara',
